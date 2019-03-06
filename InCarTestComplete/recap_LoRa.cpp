9ca2f00b6ec5dff113f75a883e9e5b4e093b6b1e
Ryan Swift <r926279438@gmail.com>
2019-03-06 01:59:46 -0500

    Added an 8-bit CRC to identify packets that have been corrupted

--- InCarTestComplete/recap_LoRa.cpp
+++ InCarTestComplete/recap_LoRa.cpp
@@ -2,6 +2,24 @@
 
 int packetsReceived;
 
+//CRC-8 - based on the CRC8 formulas by Dallas/Maxim
+//code released under the therms of the GNU GPL 3.0 license
+uint8_t CRC8(const uint8_t *data, uint8_t len) {
+  uint8_t crc = 0x00;
+  while (len--) {
+    uint8_t extract = *data++;
+    for (uint8_t tempI = 8; tempI; tempI--) {
+      uint8_t sum = (crc ^ extract) & 0x01;
+      crc >>= 1;
+      if (sum) {
+        crc ^= 0x8C;
+      }
+      extract >>= 1;
+    }
+  }
+  return crc;
+}
+
 void onReceive(int packetSize) {
   if (packetSize != 0) {
     packetsReceived++;
@@ -31,11 +49,16 @@ int transmitLoRa(Car_t* car)
     return -1;
   }
 
+  uint8_t crc = CRC8((uint8_t*)car, sizeof(Car_t));
+
   LoRa.beginPacket();
   LoRa.write((uint8_t*)car, sizeof(Car_t));
+  LoRa.write(crc);
   LoRa.waitCAD();
  
   car->sequence++;
+
+  LoRa.receive(); // Resume Receiving after Transmit
 
   return 0;
 }
@@ -46,9 +69,17 @@ int receiveLoRa(Car_t* car)
     return -1;
   }
 
-  if(LoRa.read((uint8_t*)car, sizeof(Car_t))==sizeof(Car_t)){
-    return sizeof(Car_t);
-  } else {
-    return -1;
+  Car_t tmp;
+
+  if(packetsReceived > 0) {
+    packetsReceived--;
+    if(LoRa.read((uint8_t*)&tmp, sizeof(Car_t)) > 0){
+      uint8_t crc = (uint8_t)LoRa.read();
+      if(CRC8((uint8_t*)&tmp, sizeof(Car_t))==crc) {
+        *car = tmp;
+        return sizeof(Car_t);
+      }
+    }
   }
+  return -1;
 }
